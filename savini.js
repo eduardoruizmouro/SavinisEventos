adfb49aa40e2fbc57e98014aeb05e66620caec12
eduardoruizmouro <ruizmouroeduardo@gmail.com>
2022-01-12 14:26:32 -0300

    sistema de cadastro de participantes finalizado

---
+++ savini.js
@@ -0,0 +1,24 @@
+let dataEvento = 14/01/2022
+
+if (dataEvento >= 12/01/2022) {
+    console.log ("data do evento posterior a 12/01/2022 , permitir cadastro") 
+}  else {console.log ("data do evento anterior ,nao permitir cadastro")
+
+}
+
+let idadeParticipante = 19
+
+if (idadeParticipante >=18){
+    console.log ("idade maior de 18 ,permitir cadastro")
+}  else {console.log ("idade menor de 18, proibido o cadastro")
+
+}
+
+let quantidadeParticipante =101   
+
+if (quantidadeParticipante <=100){
+    console.log ("quantidade menor que 100, pode participar")
+}  else {console.log("quantidade maior nao pode participar")
+
+}
+  
