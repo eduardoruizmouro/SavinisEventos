78c6a618cf91d5bd911b49a25e0bae40c5611925
eduardoruizmouro <ruizmouroeduardo@gmail.com>
2022-01-30 12:12:57 -0300

    novo codigo

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 # Criar um sistema de cadastro para eventos.
 # Criar um fluxograma com as etapas do projeto.
 # Implementar linguagem de programação no sistema.
+# Alterar o codigo
+# Nova Alteração no código
--- savini.js
+++ savini.js
@@ -1,24 +1,8 @@
-let dataEvento = 14/01/2022
+// node
 
-if (dataEvento >= 12/01/2022) {
-    console.log ("data do evento posterior a 12/01/2022 , permitir cadastro") 
-}  else {console.log ("data do evento anterior ,nao permitir cadastro")
+let listaParticipantes = ["Roberto","Carlos","Maria","Joana"]
 
+for (let contador=0;contador > listaParticipantes.length;contador++){ 
+    console.log(listaParticipantes[contador])
 }
 
-let idadeParticipante = 19
-
-if (idadeParticipante >=18){
-    console.log ("idade maior de 18 ,permitir cadastro")
-}  else {console.log ("idade menor de 18, proibido o cadastro")
-
-}
-
-let quantidadeParticipante =101   
-
-if (quantidadeParticipante <=100){
-    console.log ("quantidade menor que 100, pode participar")
-}  else {console.log("quantidade maior nao pode participar")
-
-}
-  
