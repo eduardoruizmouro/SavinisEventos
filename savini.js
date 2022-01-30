dfc2db56c2ea6a3f55351370daff6941e31a4b58
eduardoruizmouro <ruizmouroeduardo@gmail.com>
2022-01-30 12:55:05 -0300

    acrescentando alteracoes

--- savini.js
+++ savini.js
@@ -1,8 +1,19 @@
-// node
+
 
 let listaParticipantes = ["Roberto","Carlos","Maria","Joana"]
 
 for (let contador=0;contador > listaParticipantes.length;contador++){ 
     console.log(listaParticipantes[contador])
+}
+
+let dataEvento = new Date ('02/02/2022')
+let dataHoje = new Date ()
+
+console.log (dataEvento)
+if (dataEvento > dataHoje){
+    console.log ("Evento Válido")
+} else {
+    console.log ("Evento Inválido")
+
 }
 
