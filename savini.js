bee53306a7ba2d5dfb3639b9993d9c3170815cb2
eduardoruizmouro <ruizmouroeduardo@gmail.com>
2022-01-31 10:10:41 -0300

    Complemento do Código

--- savini.js
+++ savini.js
@@ -1,10 +1,5 @@
 
 
-let listaParticipantes = ["Roberto","Carlos","Maria","Joana"]
-
-for (let contador=0;contador > listaParticipantes.length;contador++){ 
-    console.log(listaParticipantes[contador])
-}
 
 let dataEvento = new Date ('02/02/2022')
 let dataHoje = new Date ()
@@ -16,4 +11,23 @@ if (dataEvento > dataHoje){
     console.log ("Evento Inválido")
 
 }
+let idadeParticipante = 19
 
+if (idadeParticipante >=18){
+    console.log ("idade maior de 18 ,permitir cadastro")
+}  else {console.log ("idade menor de 18, proibido o cadastro")
+
+}
+let listaParticipantes = ["Roberto","Carlos","Maria","Joana"]
+
+for (let contador = 0 ; contador < listaParticipantes.length; contador++) { 
+    console.log(listaParticipantes[contador])
+    
+}
+let quantidadeParticipante =101   
+
+if (quantidadeParticipante <=100){
+    console.log ("quantidade menor que 100, pode participar")
+}  else {console.log("quantidade maior nao pode participar")
+
+}
